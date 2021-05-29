e4436ef9cf9cd5ae6daddcd2bc1dfbb18f16f96c
alex hj <ingookey@163.com>
2021-05-29 22:52:16 +0800

    2252

--- src/com/parseyaml/main.py
+++ src/com/parseyaml/main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import yaml
+import sqlite3
 
 
 class YamlHandler:
@@ -34,6 +35,74 @@ def parse_config(section_product):
     return None
 
 
+class LocalSql(object):
+    def __init__(self, name_db='local.db', name_table='local'):
+        self.name_db = os.path.join(os.path.dirname(os.path.abspath("__file__")), name_db)
+        self.name_table = name_table
+        print(self.name_db)
+
+    def init_db(self):
+        conn = sqlite3.connect(self.name_db)
+        cursor = conn.cursor()
+        sql = '''SELECT tbl_name FROM sqlite_master WHERE type = 'table' '''
+        cursor.execute(sql)
+        values = cursor.fetchall()
+        tables = []
+        for item in values:
+            tables.append(item[0])
+
+        if self.name_table not in tables:
+            sql = '''CREATE TABLE IF NOT EXISTS ''' + self.name_table + ''' (
+                _id INTEGER PRIMARY KEY AUTOINCREMENT,
+                product TEXT,
+                config_file TEXT,
+                weiht FLOAT
+                )'''
+            cursor.execute(sql)
+            cursor.close()
+            print(self.name_table + ' 创建成功')
+        else:
+            print(self.name_table + ' 已经存在')
+        pass
+
+    def update_db(self):
+        conn = sqlite3.connect(self.name_db)
+        cursor = conn.cursor()
+        insert_data = [('pt1', 'pt1.yaml', 3.0), ('pt2', 'pt2.yaml', 4.0)]
+
+        #cursor.execute("INSERT INTO {} (product, config_file, weiht) \
+        #    VALUES ('pt1', 'pt1.yaml', 1.0)".format(self.name_table))
+        #cursor.execute("INSERT INTO {} (product, config_file, weiht) \
+        #    VALUES ('pt2', 'pt2.yaml', 2.0)".format(self.name_table))
+        if True:
+            sql = "INSERT INTO {} (product, config_file, weiht) VALUES (?, ?, ?)".format(self.name_table)
+            cursor.executemany(sql, insert_data)
+        sql = "UPDATE {} SET config_file=?, weiht=? WHERE product=?".format(self.name_table)
+        cursor.execute(sql, ('pt1.yaml', 7, 'pt1'))
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+    def query_db(self):
+        conn = sqlite3.connect(self.name_db)
+        cursor = conn.cursor()
+        sql = "select * from {}".format(self.name_table)
+        #sql = "select product, config_file from {}".format(self.name_table)
+        results = cursor.execute(sql)
+        list_results = results.fetchall()
+        if list_results:
+            for item in list_results:
+                print("result: {}".format(item))
+        cursor.close()
+        conn.close()
+
+def test():
+    ls = LocalSql()
+    ls.init_db()
+    ls.update_db()
+    ls.query_db()
+    pass
+
 if __name__ == '__main__':
     list_para = sys.argv[1:]
     para_product = ""
@@ -41,15 +110,17 @@ if __name__ == '__main__':
         para_product = list_para[0]
 
     file_yaml = parse_config(para_product)
+    test()
     if not file_yaml:
         sys.exit(1)
     cur_path = os.path.dirname(os.path.realpath(__file__))
     yaml_path = os.path.join(cur_path, file_yaml)
     file_path = os.path.join(os.path.dirname(os.path.abspath("__file__")), file_yaml)
+    print(file_path, os.path.abspath("__file__"))
     yaml_fp = YamlHandler(yaml_path)
     yaml_data = yaml_fp.get_ymal_data()
 
-    if True:
+    if False:
         for it in range(len(yaml_data['SIGN_FILE_LIST'])):
             for item in yaml_data['SIGN_FILE_LIST'][it].values():
                 if item is not None:
