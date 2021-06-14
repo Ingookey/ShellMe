66e6662b7145d8a594c79c2f499f5881fa2e44bb
alex hj <ingookey@163.com>
2021-06-14 18:05:28 +0800

    1805

--- cprojs/main.c
+++ cprojs/main.c
@@ -1,10 +1,33 @@
+//#include "./easylogger/inc/elog.h"
+//#include "./easylogger/inc/elog_cfg.h"
 #include "demo.h"
 #include "leecode.h"
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
+    ///* close printf buffer */
+    // setbuf(stdout, NULL);
+    ///* initialize EasyLogger */
+    // elog_init();
+    ///* set EasyLogger log format */
+    // elog_set_fmt(ELOG_LVL_ASSERT, ELOG_FMT_ALL);
+    // elog_set_fmt(ELOG_LVL_ERROR, ELOG_FMT_LVL | ELOG_FMT_TAG |
+    // ELOG_FMT_TIME); elog_set_fmt(ELOG_LVL_WARN, ELOG_FMT_LVL | ELOG_FMT_TAG |
+    // ELOG_FMT_TIME); elog_set_fmt(ELOG_LVL_INFO, ELOG_FMT_LVL | ELOG_FMT_TAG |
+    // ELOG_FMT_TIME); elog_set_fmt(ELOG_LVL_DEBUG, ELOG_FMT_ALL &
+    // ~ELOG_FMT_FUNC); elog_set_fmt(ELOG_LVL_VERBOSE, ELOG_FMT_ALL &
+    // ~ELOG_FMT_FUNC);
+    ///* start EasyLogger */
+    // elog_start();
+
     int (*fp)(int *pl, int size);
     int numbers[] = {40, 52, 71, 30, 18, 89};
+    // log_a("Hello EasyLogger!");
+    // log_e("Hello EasyLogger!");
+    // log_w("Hello EasyLogger!");
+    // log_i("Hello EasyLogger!");
+    // log_d("Hello EasyLogger!");
+    // log_v("Hello EasyLogger!");
 
 #if 0
     int *pResult = NULL;
